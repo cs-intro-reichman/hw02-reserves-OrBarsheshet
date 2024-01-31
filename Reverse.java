3cc33590bda3dddf459923e098eb84b7a276742b
OrBarsheshet <or.barsheshet@post.runi.ac.il>
2024-01-31 20:07:37 +0200

    second try- Reverse

--- Reverse.java
+++ Reverse.java
@@ -5,11 +5,13 @@
 public class Reverse {
 	public static void main (String[] args){
 		String Word = args[0];
+		String NewWord = "";
         for (int i=Word.length()-1; i>=0; i--){
             System.out.print(Word.charAt(i));
+			NewWord = NewWord + Word.charAt(i);
         }
         System.out.println();
-        int x= Word.length();
-        System.out.println("The middle character is " + Word.charAt((x/2)));
+        int x= NewWord.length();
+        System.out.println("The middle character is " + NewWord.charAt((x/2)));
 	}
 }
